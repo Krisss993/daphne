003041a6b9056c31c74b9dcc71f6413d211a4217
kpas9 <kristopherp994@gmail.com>
2024-09-06 19:47:35 +0200

    First release

--- manage.py
+++ manage.py
@@ -17,6 +17,9 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+    # Get the PORT environment variable
+    port = os.environ.get('PORT', '8000')
+    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + port])
 
 if __name__ == '__main__':
     main()
