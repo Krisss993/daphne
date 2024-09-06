928472916016196ff849cd8dcc0a2fc2a62ad4ff
kpas9 <kristopherp994@gmail.com>
2024-09-06 18:41:42 +0200

    First release

--- Django_React_Langchain_Stream/settings.py
+++ Django_React_Langchain_Stream/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+# Load environment variables from the .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cb&rv%$mtvi-yw-k+c#da_*mf18e&ftwx+71&37v@d(npdf$%2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
