9ca5113c47ea456c96d44e40cd6fb7d4f7e81d9b
kpas9 <kristopherp994@gmail.com>
2024-09-17 14:15:38 +0200

    First deploy

--- Django_React_Langchain_Stream/settings.py
+++ Django_React_Langchain_Stream/settings.py
@@ -1,4 +1,6 @@
 import os
+import dj_database_url
+
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -101,13 +103,16 @@ ASGI_APPLICATION = "Django_React_Langchain_Stream.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
